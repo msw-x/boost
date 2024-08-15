1b3ce3fec8b0a9209ad6fe4bcd78709760a2b07e
msw <mswl@ya.ru>
2024-08-15 17:28:48 +0300

    fix fmt int

--- xfmt/int.go
+++ xfmt/int.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/msw-x/boost/xstring"
 	"golang.org/x/exp/constraints"
 )
 
@@ -33,6 +32,8 @@ type fmtInt[V constraints.Integer] struct {
 }
 
 func (o *fmtInt[V]) parse(f string) (err error) {
+	o.base = 1000
+	o.precision = 1
 	r := strings.NewReader(f)
 	ps := ""
 	rank := &o.minRank
@@ -46,21 +47,24 @@ func (o *fmtInt[V]) parse(f string) (err error) {
 			case ' ':
 				o.space = " "
 			case '.':
+				o.maxRank = len(UnitPrefix) + 1
 			case ':':
 				rank = &o.maxRank
 			default:
 				if '0' <= b && b <= '9' {
 					ps += string(b)
 				} else {
-					i := slices.Index([]byte(UnitPrefix), b)
-					if i == -1 {
+					i := slices.Index([]byte(UnitPrefix), b) + 1
+					if i == 0 {
 						break
 					} else {
 						*rank = i
+						if rank == &o.minRank {
+							o.maxRank = i
+						}
 					}
 				}
 			}
-			fmt.Println(string(b))
 		} else {
 			if err == io.EOF {
 				err = nil
@@ -80,6 +84,10 @@ func (o *fmtInt[V]) parse(f string) (err error) {
 }
 
 func (o *fmtInt[V]) convert(v V) (s string) {
+	negative := v < 0
+	if negative {
+		v = -v
+	}
 	i := v
 	rank := 0
 	base := V(o.base)
@@ -91,15 +99,15 @@ func (o *fmtInt[V]) convert(v V) (s string) {
 		i /= base
 		rank++
 	}
-	if len(o.space) == 0 {
-		s = fmt.Sprintf("%d", i)
-	} else {
-		s = wideInt(i, o.space)
-	}
+	tail := ""
 	if o.precision > 0 {
 		div := V(math.Pow(float64(base), float64(rank)))
-		f := float64(v) / float64(div)
-		_, fract := xstring.Pair(fmt.Sprintf(fmt.Sprintf("%%.%df", o.precision), f), ".")
+		f := float64(v)/float64(div) - float64(i)
+		g := fmt.Sprintf(fmt.Sprintf("%%.%df", o.precision), f)
+		fract := g[2:]
+		if g[0] == '1' {
+			i++
+		}
 		if !o.fixed {
 			ok := true
 			for ok {
@@ -107,13 +115,20 @@ func (o *fmtInt[V]) convert(v V) (s string) {
 			}
 		}
 		if len(fract) > 0 {
-			s = fmt.Sprintf("%s.%s", s, fract)
+			tail = "." + fract
 		}
 	}
-	prefix := ""
-	if rank > 0 {
-		prefix = string(UnitPrefix[rank-1])
+	if len(o.space) == 0 {
+		s = fmt.Sprintf("%d", i)
+	} else {
+		s = wideInt(i, o.space)
 	}
-	s = fmt.Sprintf("%s%s%s", s, o.space, prefix)
+	s += tail
+	if rank > 0 {
+		s += o.space + string(UnitPrefix[rank-1])
+	}
+	if negative {
+		s = "-" + s
+	}
 	return
 }
