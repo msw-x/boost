0b7c0d1a16a7ba7a63730b66bb1069708e348d9f
msw <mswl@ya.ru>
2024-08-15 15:47:39 +0300

    hash

--- xstring/hash.go
+++ xstring/hash.go
@@ -3,18 +3,16 @@ package xstring
 import (
 	"crypto/md5"
 	"crypto/sha1"
-
-	"github.com/msw-x/boost/xfmt"
 )
 
-func Md5(s string) string {
+func Md5(s string) []byte {
 	h := md5.New()
 	h.Write([]byte(s))
-	return xfmt.Hex(h.Sum(nil))
+	return h.Sum(nil)
 }
 
-func Sha1(s string) string {
+func Sha1(s string) []byte {
 	h := sha1.New()
 	h.Write([]byte(s))
-	return xfmt.Hex(h.Sum(nil))
+	return h.Sum(nil)
 }
