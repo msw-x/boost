42d1421fcd3ad09578474e791b8688de9443e8ee
msw <mswl@ya.ru>
2024-09-30 10:43:22 +0300

    scale

--- xmath/scale.go
+++ xmath/scale.go
@@ -7,11 +7,11 @@ import (
 // Order of magnitude
 func Scale[T Number](v T) int {
 	if v == 0 {
-		return 0
+		return 1 // undefined
 	}
 	f := math.Abs(float64(v))
 	n := int(math.Floor(math.Log10(f)))
-	if f > 1 {
+	if f >= 1 {
 		n++
 	}
 	return n
