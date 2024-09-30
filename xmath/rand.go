24ef1b51dfab545480c329355f10a404b3561462
msw <mswl@ya.ru>
2024-09-30 10:43:14 +0300

    rand

--- xmath/rand.go
+++ xmath/rand.go
@@ -2,6 +2,7 @@ package xmath
 
 import "math/rand"
 
+// Rand returns, as an int, a non-negative pseudo-random number in the half-open interval [min,max]
 func Rand(min, max int) int {
-	return rand.Intn(max-min) + min
+	return rand.Intn(max-min+1) + min
 }
